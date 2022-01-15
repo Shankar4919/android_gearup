cdf34b92ee96e83f7f13f76039478918e9eec677
Shankar4919 <itsme.shankar8@gmail.com>
2022-01-15 09:36:22 +0545

    navigations

--- gearup/lib/main.dart
+++ gearup/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gearup/pages/navpages/main_page.dart';
 import 'package:gearup/pages/welcome_page.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:WelcomePage()
+      home:MainPage()
     );
   }
 }
