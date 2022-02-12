321d1e97adc20c71d4650cf277d2c219a737c8a4
Shankar4919 <itsme.shankar8@gmail.com>
2022-02-12 12:04:41 +0545

    order detail and product without cart

---
+++ gearup/lib/Layout/Views/cart/widgets/order_details.dart
@@ -0,0 +1,65 @@
+import 'package:e_commers/Bloc/product/product_bloc.dart';
+import 'package:e_commers/Layout/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class OrderDetails extends StatelessWidget {
+  const OrderDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final productBloc = BlocProvider.of<ProductBloc>(context);
+
+    return Container(
+      margin: const EdgeInsets.only(top: 10.0),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+      height: 130,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const TextGearUp(
+                text: 'Order',
+                fontSize: 19,
+              ),
+              TextGearUp(
+                text: '\$ ${productBloc.state.total}',
+                fontSize: 19,
+              )
+            ],
+          ),
+          const SizedBox(height: 10.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const TextGearUp(
+                text: 'Delivery',
+                fontSize: 19,
+              ),
+              TextGearUp(
+                text: '\$ ${productBloc.state.delivery}',
+                fontSize: 19,
+              )
+            ],
+          ),
+          const SizedBox(height: 10.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const TextGearUp(
+                text: 'Insurance',
+                fontSize: 19,
+              ),
+              TextGearUp(
+                text: '\$ ${productBloc.state.insurance}',
+                fontSize: 19,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
